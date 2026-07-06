fe22f2078078835a0f1e5b0fd8f2eff29b98d56b
Mariano Parisi <marianoparisi59@gmail.com>
2026-07-05 22:31:23 -0300

    Reuse Tina database instance

--- tina/database.ts
+++ tina/database.ts
@@ -2,6 +2,13 @@ import { createDatabase, createLocalDatabase } from '@tinacms/datalayer';
 import { MongodbLevel } from 'mongodb-level';
 import { GitHubProvider } from 'tinacms-gitprovider-github';
 
+type TinaDatabase = ReturnType<typeof createDatabase> | ReturnType<typeof createLocalDatabase>;
+
+declare global {
+  // eslint-disable-next-line no-var
+  var __cdfTinaDatabase: TinaDatabase | undefined;
+}
+
 const branch =
   process.env.GITHUB_BRANCH ||
   process.env.NEXT_PUBLIC_TINA_BRANCH ||
@@ -10,19 +17,24 @@ const branch =
 
 const isLocal = process.env.TINA_PUBLIC_IS_LOCAL === 'true';
 
-export default isLocal
-  ? createLocalDatabase()
-  : createDatabase({
-      gitProvider: new GitHubProvider({
-        branch,
-        owner: process.env.GITHUB_OWNER as string,
-        repo: process.env.GITHUB_REPO as string,
-        token: process.env.GITHUB_PERSONAL_ACCESS_TOKEN as string,
-      }),
-      databaseAdapter: new MongodbLevel<string, Record<string, any>>({
-        collectionName: `tinacms-${branch}`,
-        dbName: process.env.MONGODB_DB_NAME || 'tina_cdf',
-        mongoUri: process.env.MONGODB_URI as string,
-      }),
-      namespace: branch,
-    });
+const createTinaDatabase = () =>
+  isLocal
+    ? createLocalDatabase()
+    : createDatabase({
+        gitProvider: new GitHubProvider({
+          branch,
+          owner: process.env.GITHUB_OWNER as string,
+          repo: process.env.GITHUB_REPO as string,
+          token: process.env.GITHUB_PERSONAL_ACCESS_TOKEN as string,
+        }),
+        databaseAdapter: new MongodbLevel<string, Record<string, any>>({
+          collectionName: `tinacms-${branch}`,
+          dbName: process.env.MONGODB_DB_NAME || 'tina_cdf',
+          mongoUri: process.env.MONGODB_URI as string,
+        }),
+        namespace: branch,
+      });
+
+globalThis.__cdfTinaDatabase ||= createTinaDatabase();
+
+export default globalThis.__cdfTinaDatabase;
